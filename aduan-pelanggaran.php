9b0580a43909ea45ae582ce4fe3ca8ec813039da
Dzulfikar Muhammad Al Ghifari <fikaralghifari0504@gmail.com>
2024-11-28 12:27:15 +0700

    fix roles

--- aduan-pelanggaran.php
+++ aduan-pelanggaran.php
@@ -2,6 +2,10 @@
 session_start();
 $_SESSION['menu_active'] = 'aduan-pelanggaran';
 
+if ( $_SESSION['user']['role'] == 4) {
+    header("location:/tataTertib");
+}
+
 include 'layout/header.php';
 include 'pages/aduanPelanggaran/index.php';
 include 'layout/footer.php';
