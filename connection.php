fb8cc1f114455be8f2cbfc378f04649e85841817
Dzulfikar Muhammad Al Ghifari <fikaralghifari0504@gmail.com>
2024-11-29 18:32:33 +0700

    test

--- connection.php
+++ connection.php
@@ -2,5 +2,7 @@
 $serverName = "DESKTOP-I0JL8UH\DBMS1"; 
 
 $connectionInfo = array( "Database"=>"tatatertib");
+
+
 // $connectionInfo = array( "Database"=>"tataTertibNew");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
